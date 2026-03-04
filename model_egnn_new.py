62c1260aa8bb8d57c2d6a509f0b208002f710bfa
HaaZheng <163391767+HaaZheng@users.noreply.github.com>
2026-03-04 16:44:22 +0800

    Add files via upload

---
+++ model_egnn_new.py
@@ -0,0 +1,350 @@
+import torch
+import torch.nn as nn
+
+from torch_geometric.nn import DenseGCNConv, GCNConv, global_mean_pool as gep
+from torch_geometric.utils import dropout_adj
+
+
+def unsorted_segment_sum(data, segment_ids, num_segments):
+    result_shape = (num_segments, data.size(1))
+    result = data.new_full(result_shape, 0)  # Init empty result tensor.
+    segment_ids = segment_ids.unsqueeze(-1).expand(-1, data.size(1))
+    result.scatter_add_(0, segment_ids, data)
+    return result
+
+def unsorted_segment_mean(data, segment_ids, num_segments):
+    result_shape = (num_segments, data.size(1))
+    segment_ids = segment_ids.unsqueeze(-1).expand(-1, data.size(1))
+    result = data.new_full(result_shape, 0)  # Init empty result tensor.
+    count = data.new_full(result_shape, 0)
+    result.scatter_add_(0, segment_ids, data)
+    count.scatter_add_(0, segment_ids, torch.ones_like(data))
+    return result / count.clamp(min=1)
+
+class EGNN(nn.Module):
+    """
+    E(n) Equivariant Convolutional Layer
+    re
+    """
+
+    def __init__(self, input_nf, output_nf, hidden_nf, edges_in_d=3, act_fn=nn.SiLU(), residual=None, attention=False, normalize=False, coords_agg='mean', tanh=False):
+        super(EGNN, self).__init__()
+        input_edge = input_nf * 2
+        self.residual = residual
+        self.attention = attention
+        self.normalize = normalize
+        self.coords_agg = coords_agg
+        self.tanh = tanh
+        self.epsilon = 1e-8
+        edge_coords_nf = 1
+
+        self.edge_mlp = nn.Sequential(
+            nn.Linear(input_edge + edge_coords_nf + edges_in_d, hidden_nf),
+            act_fn,
+            nn.Linear(hidden_nf, hidden_nf),
+            act_fn)
+
+        self.node_mlp = nn.Sequential(
+            nn.Linear(hidden_nf + input_nf, hidden_nf),
+            act_fn,
+            nn.Linear(hidden_nf, output_nf))
+
+        layer = nn.Linear(hidden_nf, 1, bias=False)
+        torch.nn.init.xavier_uniform_(layer.weight, gain=0.001)
+
+        coord_mlp = []
+        coord_mlp.append(nn.Linear(hidden_nf, hidden_nf))
+        coord_mlp.append(act_fn)
+        coord_mlp.append(layer)
+        if self.tanh:
+            coord_mlp.append(nn.Tanh())
+        self.coord_mlp = nn.Sequential(*coord_mlp)
+
+        if self.attention:
+            self.att_mlp = nn.Sequential(
+                nn.Linear(hidden_nf, 1),
+                nn.Sigmoid())
+
+    def edge_model(self, source, target, radial, edge_attr):
+        if edge_attr is None:  # Unused.
+            out = torch.cat([source, target, radial], dim=1)
+        else:
+            out = torch.cat([source, target, radial, edge_attr], dim=1)
+        out = self.edge_mlp(out)
+        if self.attention:
+            att_val = self.att_mlp(out)
+            out = out * att_val
+        return out
+
+    def node_model(self, x, edge_index, edge_attr, node_attr):
+        row, col = edge_index
+        agg = unsorted_segment_sum(edge_attr, row, num_segments=x.size(0))
+        if node_attr is not None:
+            agg = torch.cat([x, agg, node_attr], dim=1)
+        else:
+            agg = torch.cat([x, agg], dim=1)
+
+        out = self.node_mlp(agg)
+        if self.residual:
+            out = x + out
+        return out, agg
+
+    def coord_model(self, coord, edge_index, coord_diff, edge_feat):
+        row, col = edge_index
+        trans = coord_diff * self.coord_mlp(edge_feat)
+        if self.coords_agg == 'sum':
+            agg = unsorted_segment_sum(trans, row, num_segments=coord.size(0))
+        elif self.coords_agg == 'mean':
+            agg = unsorted_segment_mean(trans, row, num_segments=coord.size(0))
+        else:
+            raise Exception('Wrong coords_agg parameter' % self.coords_agg)
+        coord = coord + agg
+        return coord
+
+    def coord2radial(self, edge_index, coord):
+        row, col = edge_index
+        coord_diff = coord[row] - coord[col]
+        radial = torch.sum(coord_diff**2, 1).unsqueeze(1)
+
+        if self.normalize:
+            norm = torch.sqrt(radial).detach() + self.epsilon
+            coord_diff = coord_diff / norm
+
+        return radial, coord_diff
+
+    def forward(self, h, edge_index, coord, edge_attr, node_attr=None):
+
+        row, col = edge_index
+        radial, coord_diff = self.coord2radial(edge_index, coord)
+
+        edge_feat = self.edge_model(h[row], h[col], radial, edge_attr)
+        coord = self.coord_model(coord, edge_index, coord_diff, edge_feat)
+
+        h, agg = self.node_model(h, edge_index, edge_feat, node_attr)
+
+        # return h, coord, edge_attr
+        return h
+
+
+class EGNNBlock(nn.Module):
+    def __init__(self, gcn_layers_dim, dropout_rate=0., relu_layers_index=[], dropout_layers_index=[]):
+        super(EGNNBlock, self).__init__()
+
+        self.conv_layers = nn.ModuleList()
+        for i in range(len(gcn_layers_dim) - 1):
+            # input_nf, output_nf, hidden_nf
+            conv_layer = EGNN(input_nf=gcn_layers_dim[i], output_nf=gcn_layers_dim[i + 1],hidden_nf=gcn_layers_dim[i + 1])
+            self.conv_layers.append(conv_layer)
+
+        self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(dropout_rate)
+        self.relu_layers_index = relu_layers_index
+        self.dropout_layers_index = dropout_layers_index
+
+    def forward(self, x, edge_index, edge_weight,coords,batch):
+
+        # cross-attention for node feature fusion, including drugs and proteins
+
+
+        output = x
+        embeddings = []
+        for conv_layer_index in range(len(self.conv_layers)):
+
+            output = self.conv_layers[conv_layer_index](output,edge_index,coords, edge_weight)
+            if conv_layer_index in self.relu_layers_index:
+                output = self.relu(output)
+            if conv_layer_index in self.dropout_layers_index:
+                output = self.dropout(output)
+            embeddings.append(gep(output, batch))
+
+        return embeddings
+
+
+class EGNNModel(nn.Module):
+    def __init__(self, layers_dim):
+        super(EGNNModel, self).__init__()
+
+        self.num_layers = len(layers_dim) - 1
+        self.graph_conv = EGNNBlock(layers_dim, relu_layers_index=list(range(self.num_layers)))
+
+    def forward(self, graph_batchs):
+
+        embedding_batchs = list(
+            #  x, edge_index, edge_weight,coords,batch
+                map(lambda graph: self.graph_conv(graph.x,graph.edge_index, graph.edge_weight,graph.coords,graph.batch), graph_batchs))
+        embeddings = []
+        for i in range(self.num_layers):
+            embeddings.append(torch.cat(list(map(lambda embedding_batch: embedding_batch[i], embedding_batchs)), 0))
+
+        return embeddings
+
+
+class DenseGCNBlock(nn.Module):
+    def __init__(self, gcn_layers_dim, dropout_rate=0., relu_layers_index=[], dropout_layers_index=[]):
+        super(DenseGCNBlock, self).__init__()
+
+        self.conv_layers = nn.ModuleList()
+        for i in range(len(gcn_layers_dim) - 1):
+            conv_layer = DenseGCNConv(gcn_layers_dim[i], gcn_layers_dim[i + 1])
+            self.conv_layers.append(conv_layer)
+
+        self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(dropout_rate)
+        self.relu_layers_index = relu_layers_index
+        self.dropout_layers_index = dropout_layers_index
+
+    def forward(self, x, adj):
+        output = x
+        embeddings = []
+        for conv_layer_index in range(len(self.conv_layers)):
+            output = self.conv_layers[conv_layer_index](output, adj, add_loop=False)
+            if conv_layer_index in self.relu_layers_index:
+                output = self.relu(output)
+            if conv_layer_index in self.dropout_layers_index:
+                output = self.dropout(output)
+            embeddings.append(torch.squeeze(output, dim=0))
+
+        return embeddings
+
+
+class DenseGCNModel(nn.Module):
+    def __init__(self, layers_dim, edge_dropout_rate=0.):
+        super(DenseGCNModel, self).__init__()
+
+        self.edge_dropout_rate = edge_dropout_rate
+        self.num_layers = len(layers_dim) - 1
+        self.graph_conv = DenseGCNBlock(layers_dim, 0.1, relu_layers_index=list(range(self.num_layers)),
+                                        dropout_layers_index=list(range(self.num_layers)))
+
+    def forward(self, graph):
+        xs, adj, num_d, num_t = graph.x, graph.adj, graph.num_drug, graph.num_target
+        indexs = torch.where(adj != 0)
+        edge_indexs = torch.cat((torch.unsqueeze(indexs[0], 0), torch.unsqueeze(indexs[1], 0)), 0)
+        edge_indexs_dropout, edge_weights_dropout = dropout_adj(edge_index=edge_indexs, edge_attr=adj[indexs],
+                                                                p=self.edge_dropout_rate, force_undirected=True,
+                                                                num_nodes=num_d + num_t, training=self.training)
+        adj_dropout = torch.zeros_like(adj)
+        adj_dropout[edge_indexs_dropout[0], edge_indexs_dropout[1]] = edge_weights_dropout
+
+        embeddings = self.graph_conv(xs, adj_dropout)
+
+        return embeddings
+
+
+class LinearBlock(nn.Module):
+    def __init__(self, linear_layers_dim, dropout_rate=0., relu_layers_index=[], dropout_layers_index=[]):
+        super(LinearBlock, self).__init__()
+
+        self.layers = nn.ModuleList()
+        for i in range(len(linear_layers_dim) - 1):
+            layer = nn.Linear(linear_layers_dim[i], linear_layers_dim[i + 1])
+            self.layers.append(layer)
+
+        self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(dropout_rate)
+        self.relu_layers_index = relu_layers_index
+        self.dropout_layers_index = dropout_layers_index
+
+    def forward(self, x):
+        output = x
+        embeddings = []
+        for layer_index in range(len(self.layers)):
+            output = self.layers[layer_index](output)
+            if layer_index in self.relu_layers_index:
+                output = self.relu(output)
+            if layer_index in self.dropout_layers_index:
+                output = self.dropout(output)
+            embeddings.append(output)
+
+        return embeddings
+
+
+class Contrast(nn.Module):
+    def __init__(self, hidden_dim, output_dim, tau, lam):
+        super(Contrast, self).__init__()
+
+        self.proj = nn.Sequential(
+            nn.Linear(hidden_dim, hidden_dim),
+            nn.ELU(),
+            nn.Linear(hidden_dim, output_dim))
+        self.tau = tau
+        self.lam = lam
+        for model in self.proj:
+            if isinstance(model, nn.Linear):
+                nn.init.xavier_normal_(model.weight, gain=1.414)
+
+    def sim(self, z1, z2):
+        z1_norm = torch.norm(z1, dim=-1, keepdim=True)
+        z2_norm = torch.norm(z2, dim=-1, keepdim=True)
+        dot_numerator = torch.mm(z1, z2.t())
+        dot_denominator = torch.mm(z1_norm, z2_norm.t())
+        sim_matrix = torch.exp(dot_numerator / dot_denominator / self.tau)
+
+        return sim_matrix
+
+    def forward(self, za, zb, pos):
+        za_proj = self.proj(za)
+        zb_proj = self.proj(zb)
+        matrix_a2b = self.sim(za_proj, zb_proj)
+        matrix_b2a = matrix_a2b.t()
+
+        matrix_a2b = matrix_a2b / (torch.sum(matrix_a2b, dim=1).view(-1, 1) + 1e-8)
+        lori_a = -torch.log(matrix_a2b.mul(pos.to_dense()).sum(dim=-1)).mean()
+
+        matrix_b2a = matrix_b2a / (torch.sum(matrix_b2a, dim=1).view(-1, 1) + 1e-8)
+        lori_b = -torch.log(matrix_b2a.mul(pos.to_dense()).sum(dim=-1)).mean()
+
+        return self.lam * lori_a + (1 - self.lam) * lori_b, torch.cat((za_proj, zb_proj), 1)
+
+
+class CSCoDTA(nn.Module):
+    def __init__(self, tau, lam, ns_dims, d_ms_dims, t_ms_dims, embedding_dim=128, dropout_rate=0.2):
+        super(CSCoDTA, self).__init__()
+
+        self.output_dim = embedding_dim * 2
+
+        self.affinity_graph_conv = DenseGCNModel(ns_dims, dropout_rate)
+        # self.drug_graph_conv = GCNModel(d_ms_dims)
+        # self.target_graph_conv = GCNModel(t_ms_dims)
+
+        self.drug_graph_conv = EGNNModel(d_ms_dims)
+        self.target_graph_conv = EGNNModel(t_ms_dims)
+
+        self.drug_contrast = Contrast(ns_dims[-1], embedding_dim, tau, lam)
+        self.target_contrast = Contrast(ns_dims[-1], embedding_dim, tau, lam)
+
+    def forward(self, affinity_graph, drug_graph_batchs, target_graph_batchs, drug_pos, target_pos):
+        num_d = affinity_graph.num_drug
+
+        affinity_graph_embedding = self.affinity_graph_conv(affinity_graph)[-1]
+        drug_graph_embedding = self.drug_graph_conv(drug_graph_batchs)[-1]
+        target_graph_embedding = self.target_graph_conv(target_graph_batchs)[-1]
+
+        dru_loss, drug_embedding = self.drug_contrast(affinity_graph_embedding[:num_d], drug_graph_embedding, drug_pos)
+        tar_loss, target_embedding = self.target_contrast(affinity_graph_embedding[num_d:], target_graph_embedding,target_pos)
+
+        return dru_loss + tar_loss, drug_embedding, target_embedding
+        # return drug_graph_embedding, target_graph_embedding
+
+
+class PredictModule(nn.Module):
+    def __init__(self, embedding_dim=128, output_dim=1):
+        super(PredictModule, self).__init__()
+
+        self.prediction_func, prediction_dim_func = (lambda x, y: torch.cat((x, y), -1), lambda dim: 4 * dim)
+        mlp_layers_dim = [prediction_dim_func(embedding_dim), 1024, 512, output_dim]
+
+        self.mlp = LinearBlock(mlp_layers_dim, 0.1, relu_layers_index=[0, 1], dropout_layers_index=[0, 1])
+
+    def forward(self, data, drug_embedding, target_embedding):
+        drug_id, target_id, y = data.drug_id, data.target_id, data.y
+
+        drug_feature = drug_embedding[drug_id.int().cpu().numpy()]
+        target_feature = target_embedding[target_id.int().cpu().numpy()]
+
+        concat_feature = self.prediction_func(drug_feature, target_feature)
+        mlp_embeddings = self.mlp(concat_feature)
+        link_embeddings = mlp_embeddings[-2]
+        out = mlp_embeddings[-1]
+
+        return out, link_embeddings
